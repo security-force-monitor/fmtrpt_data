a28e63063ed888a83fe193b6f9f6217a14d9b7f1
tlongers <tl>
2024-07-10 11:47:42 +0100

    Re-scrape of 2018-2019 report: - add pagenumber. - scrapers overhauled to enable additional runs and keep older runs. - a key finding for this overhaul was the differences in XML output when pdftohtml runs directly on the DoS PDF and where it runs on a PDF produced from that original using GhostScript.

---
+++ scrapers/fmtrpt_2018_2019/2_clean_structure/202407101002/2_stack_em_up/src/aggregate_all_fmt_files.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Stack FMTRPT data files into single output TSV
+#
+# tl	2019-07-17
+#	2019-12-06 updated
+#	2021-08-06 updated with outfile test
+#	2022-03-25 updated to just aggregate current FY
+#
+# Script safety and debug
+
+set -eu
+shopt -s failglob
+
+# Choose a new outfile name each time data are updated
+
+outfile="fmtrpt_2018_2019_202407101002.tsv"
+
+_stack () {
+
+# Use csvkit's csvstack
+# Make a single tsv of the FMTRPT data, reconvert back to .tsv with full quoting
+# We keep the input filenames, which helps with troubleshooting later down the line
+
+csvstack --filenames --tabs input/*fmtrpt.tsv \
+	| csvformat -T -U "1"
+
+}
+
+_main () {
+
+# Checks if outfile already exits, so we can't accidentally wipe it
+
+if [ -e output/"$outfile" ] 
+	then 
+		printf "%s\n" "Warning: Output file $outfile already exists. Choose another name."
+		exit
+	else
+		_stack > output/"$outfile"
+fi
+
+}
+
+_main
+
+# To do:
+# - migrate to xsv? 
