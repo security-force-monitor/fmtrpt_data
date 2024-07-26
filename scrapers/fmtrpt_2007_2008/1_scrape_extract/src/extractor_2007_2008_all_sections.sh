5ab45b6e8bb276670e59c654392173e2bf980e7a
tlongers <tl>
2024-07-26 13:22:25 +0100

    Rescrape 2007-2008 report: - rescrape to get page numbers: - also fixed issue where countries that were listed but had no training activities did not show in the output; - perfect match between older and new data based on hash.

--- scrapers/fmtrpt_2007_2008/1_scrape_extract/src/extractor_2007_2008_all_sections.sh
+++ scrapers/fmtrpt_2007_2008/1_scrape_extract/src/extractor_2007_2008_all_sections.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Parsing FMTRPT FY 2016-2017 from PDF to a tsv
+# Parsing FMTRPT FY 2007-2008 from PDF to a tsv
 
 # tl@sfm / 2019-12-06
 # 	   2024-07-10 Update to orchestrate different runs
@@ -62,6 +62,8 @@ _cleanXML () {
 	# -- Use the analyze_xml.sh helper to find out the values to put in here;
 	# -- Exception here is creating a new page tag.
 	# - Check that we don't have have any <text> tags left.
+	# - Handle some initial structural issues caused by the <page> tag positioning.
+	# - Insert placeholder <training> item in countries where none was reported, along with explanation.
 	# - Introduce "page_number" attribute inside <training> tag, using the <page> tag as an index.
 	# - Remove redundant <page> tag before correcting cross-line nesting issues.
 	# - Correct problems with nesting that exist across line endings.
@@ -71,30 +73,38 @@ _cleanXML () {
 	cat output/"${r}"/1_pdf_xml/"${y}_${t}_fmtrpt.xml" \
 	| sed 's/^<page/<text/g ; s/page>/text>/g' \
 	| grep -e "^<text" \
-	| grep -v "font=\"0\"" \
 	| grep -v -e "^.*Qty.*$" \
+	| grep -v -e "^.*Volume I Section,*$" \
 	| grep -v -e "^.*Course Title.*$" \
-	| grep -v -e "^.*Training Location.*$" \
 	| grep -v -e "^.*Student's Unit.*$" \
-	| grep -v -e "^.*US Unit.*$" \
+	| grep -v -e "^.*>Students.*$" \
+	| grep -v -e "^.*US Units.*$" \
 	| grep -v -e "^.*Total Cost.*$" \
 	| grep -v -e "^.*Start Date.*$" \
 	| grep -v -e "^.*End Date.*$" \
+	| grep -v -e "^.*Location.*$" \
+	| grep -v -e "left=\"54\" width=\"2\"" \
+	| grep -v -e "FY 2007 Program Totals" \
+	| grep -v -e "FY 2007 Totals" \
 	| sed '{
 		s/<text.*number="\(.*\)" position.*$/<page>\1<\/page>/g
-		s/<text.*left="27".*font="1"><b>\(.*\) <\/b><\/text>/<country name="\1">%<c_name>\1<\/c_name>/g
-		s/<text.*left="27".*font="2"><b>\(.*\) <\/b><\/text>/<program name="\1">%<p_name>\1<\/p_name>/g
-		s/<text.*left="27".*font="3">\(.*\)<\/text>/<training>%<course_title>\1<\/course_title>/g
-		s/<text.*left="297".*font="3">\(.*\)<\/text>/<quantity>\1<\/quantity>/g
-		s/<text.*left="351".*font="3">\(.*\)<\/text>/<location>\1<\/location>/g
-		s/<text.*left="540".*font="3">\(.*\)<\/text>/<student_unit>\1<\/student_unit>/g
-		s/<text.*left="729".*font="3">\(.*\)<\/text>/<us_unit>\1<\/us_unit>/g
-		s/<text.*left="918".*font="3">\(.*\)<\/text>/<total_cost>\1<\/total_cost>/g
-		s/<text.*left="999".*font="3">\(.*\)<\/text>/<start_date>\1<\/start_date>/g
-		s/<text.*left="1080".*font="3">\(.*\)<\/text>/<end_date>\1<\/end_date>%<\/training>/g
+		s/<text.*left="54".*font="1">.*<b>\(.*\) <\/b>.*<\/text>/<country name="\1">%<c_name>\1<\/c_name>/g
+		s/<text.*left="54".*font="0"><b>\(.*\) <\/b><\/text>/<program name="\1">%<p_name>\1<\/p_name>/g
+		s/<text.*left="54".*font="2">\(.*\)<\/text>/<training>%<course_title>\1<\/course_title>/g
+		s/<text.*left="270".*font="2">\(.*\)<\/text>/<quantity>\1<\/quantity>/g
+		s/<text.*left="324".*font="2">\(.*\)<\/text>/<location>\1<\/location>/g
+		s/<text.*left="513".*font="2">\(.*\)<\/text>/<student_unit>\1<\/student_unit>/g
+		s/<text.*left="891".*font="2">\(.*\)<\/text>/<total_cost>\1<\/total_cost>/g
+		s/<text.*left="972".*font="2">\(.*\)<\/text>/<start_date>\1<\/start_date>/g
+		s/<text.*left="1053".*font="2">\(.*\)<\/text>/<end_date>\1<\/end_date>%<\/training>/g
 		}' \
 	| tr '%' '\n' \
 	| grep -v -e "^<text"  \
+	| perl -00pe '
+		s/<\/start_date>\n<training>/<\/start_date>\n<\/training>\n<training>/g ;
+		s/<\/c_name>\n<country /<\/c_name>\n<\/country>\n<country /g ;
+		s/<country name=\"(.*)\">\n<c_name>.*<\/c_name>\n<\/country>/<country name=\"\1\">\n<c_name>\1<\/c_name>\n<program name=\"No activities reported for \1 in 2007-2008\">\n<training>\n<course_title>No activities reported for \1 in 2007-2008<\/course_title>\n<\/training>\n<\/program>\n<\/country>/g
+		'\
 	| gawk 'BEGIN { FS = "" ;page = "" }
 		{	if ($0 ~ /^<page>[0-9]+<\/page>$/) {
         			print $0
@@ -110,20 +120,17 @@ _cleanXML () {
 	| perl -00pe '
 		s/<\/training>\n(<program name=\".*\">)/<\/training>\n<\/program>\n\1/g ;
 		s/<\/training>\n(<country name=\".*\">)/<\/training>\n<\/program>\n<\/country>\n\1/g ;
-		s/<\/training>\n<training>\n<course_title>.*<\/course_title>\n<program/<\/training>\n<\/program>\n<program/g ;
-		s/<\/training>\n<training>\n<course_title>.*<\/course_title>\n<country/<\/training>\n<\/program>\n<\/country>\n<country/g ;
-		s/<training>\n(<country name=\".*\">)/<\/program>\n<\/country>\n\1/g ;
 		s/<\/course_title>\n.*<training>/<\/course_title>/g ;
 		s/<\/training>\n.*<quantity>/<\/training>\n<training>\n<quantity>/g ;
-		s/<training>\n<course_title>.*<\/course_title>\n<course_title>.*<\/course_title>\n<course_title>.*<\/course_title>$//g ;
-		s/<training>\n<course_title>.*<\/course_title>\n<course_title>.*<\/course_title>$//g ;
-		s/<\/p_name>\n<quantity>/<\/p_name>\n<training>\n<quantity>/g' \
+		s/<\/start_date>\n.*<training>/<\/start_date>\n<\/training>\n<training>/g ;
+		s/<\/total_cost>\n.*<training>/<\/total_cost>\n<\/training>\n<training>/g ;
+		s/<\/student_unit>\n.*<training>/<\/student_unit>\n<\/training>\n<training>/g ;
+		s/<\/location>\n.*<training>/<\/location>\n<\/training>\n<training>/g ;
+		s/<\/p_name>\n<country/<\/p_name>\n<\/country>\n<country/g ;
+		'\
 	| gawk 'BEGIN{print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<countries>"};{print};END{print "</program>\n</country>\n</countries>"}' \
 	| grep -v "^$" \
 	> output/"${r}"/2_xml_refine/"${y}_${t}_fmtrpt_raw.xml"
-
-
-#	 s/<course_title> <\/course_title>\n<course_title> <\/course_title>//g' \
 
 }
 
@@ -148,7 +155,7 @@ _generateOutput () {
 	# Generate a TSV output from the XSML, clean up some spacing and tabbing cruft, and apply a header row.
 	# For explanation of use of xml ancestors: https://stackoverflow.com/questions/51988726/recursive-loop-xml-to-csv-with-xmlstarlet
 
-	xml sel -T -t -m "//training" -v "concat(ancestor::country/@name,'	',ancestor::program/@name,'	',course_title,'	',us_unit,'	',student_unit,'	',start_date,'	',end_date,'	',location,'	',quantity,'	',total_cost,'	',page_number)" -n output/"${r}"/4_xml_dedup/"${y}_${t}_fmtrpt_dedup.xml" \
+	xml sel -T -t -m "//training" -v "concat(ancestor::country/@name,'	',ancestor::program/@name,'	',course_title,'	','US unit not specified in 2007-2008 report','	',student_unit,'	',start_date,'	',end_date,'	',location,'	',quantity,'	',total_cost,'	',page_number)" -n output/"${r}"/4_xml_dedup/"${y}_${t}_fmtrpt_dedup.xml" \
 	| sed 's/ \{2,\}/ /g ; s/	 /	/g ; s/ 	/	/g' \
 	| awk -v p="${p}" 'BEGIN{print "country\tprogram\tcourse_title\tus_unit\tstudent_unit\tstart_date\tend_date\tlocation\tquantity\ttotal_cost\tpage_number\tsource"};{print $0"\t"p}' \
 	> output/"${r}"/5_xml_tsv/"${y}_${t}_fmtrpt.tsv"
@@ -180,10 +187,10 @@ _main () {
                 _setupOutputFolders
 
 		printf "%s: %s\n\n" "# Working on" "$t"
-		_progMsg "Extracting pages from PDF"
-		_extractPages
-		_progMsg "Converting PDF to XML"
-		_xmlConvert
+#		_progMsg "Extracting pages from PDF"
+#		_extractPages
+#		_progMsg "Converting PDF to XML"
+#		_xmlConvert
 #		_extractPagesXmlConvert # uses just pdftohtml rather than ghostscript step
 		_progMsg "Cleaning up XML"
 		_cleanXML
@@ -194,11 +201,10 @@ _main () {
 		_progMsg "Creating TSV output"
 		_generateOutput
 
-	done < src/fmtrpt_fy_2016_2017_sections
+	done < src/fmtrpt_fy_2007_2008_sections
 
 	printf "%s\n" "Done!"
 
 }
 
 _main
-
